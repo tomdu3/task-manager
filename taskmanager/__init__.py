74f334eaf842b90511694e4c81129148e2f26732
Tomislav Dukez <tomdu3@ymail.com>
2023-07-08 15:06:24 +0000

    add heroku setup code

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
 
+if os.environ.get('DEVELOPMENT') == 'True':
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+else:
+    uri = os.environ.get('DATABASE_URL')
+    if uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql', 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
