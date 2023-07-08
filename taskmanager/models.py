1b6a3570be4e14b3e34f4168ce79dea7f61ac752
Tomislav Dukez <tomdu3@ymail.com>
2023-07-08 11:48:15 +0000

    add task form, functionality and template

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,5 +1,6 @@
 from taskmanager import db
 
+
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
@@ -11,13 +12,15 @@ class Category(db.Model):
 
         return self.category_name
 
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete='CASCADE'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        'category.id', ondelete='CASCADE'), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
